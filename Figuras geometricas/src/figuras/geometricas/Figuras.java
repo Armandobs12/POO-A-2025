fffcc98b0a4da97e5f891bd70e81be4695687def
Armandobs12 <l24070011@huetamo.tecnm.mx>
2025-03-11 08:47:36 -0600

    creacion de clases de figuras
    se aplico la herencia de las clases de la figura

---
+++ Figuras geometricas/src/figuras/geometricas/Figuras.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package figuras.geometricas;
+
+/**
+ *
+ * @author ARMANDO BETANCOURT S
+ */
+public class Figuras {
+    private float area;
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+    
+    
+    public void CalcularArea(){
+    }
+    
+    
+    public void CalcularPerimetro(){
+    }
+    
+    
+    
+}
---
+++ Figuras geometricas/src/figuras/geometricas/Triangulo.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package figuras.geometricas;
+
+/**
+ *
+ * @author ARMANDO BETANCOURT S
+ */
+public class Triangulo extends Figuras {
+    private float base;
+    private float altura;
+
+    public float getBase() {
+        return base;
+    }
+
+    public void setBase(float base) {
+        this.base = base;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    
+    @Override
+    public void CalcularArea(){
+        this.setArea(this.basse*this.altura/2);
+        
+    }
+            
+    
+}
