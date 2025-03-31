af2e87f65d00b14ac81d692d99afdd98e2796b52
Armandobs12 <l24070011@huetamo.tecnm.mx>
2025-03-31 08:19:58 -0600

    Se intento
    intente crear lo solicito pero muy no saber que hacer

--- CuentaSegura/src/cuentasegura/Llave.java
+++ CuentaSegura/src/cuentasegura/Llave.java
@@ -2,16 +2,40 @@ package cuentasegura;
 
 /**
  *
- * @author Carlos
+ * @author Carlos y modificado por armando 
  */
-public class Llave {
+public class Llave extends LlavePrivada{
     private String codigo;
+    private int llaveNumerica;
+    private String numeroControl;
     
     public String generarLlave(String codigo){
         return this.codigo;
     }
+  
+ 
     
+    
+    public int procesarCadena(String cadena) {
+        int suma = 0;
+        for (char c : numeroControl.toCharArray()) {
+            if (Character.isDigit(c)) {
+                suma += Character.getNumericValue(c);
+            }
+        }
+        return suma;
+    }
+
+   
+
+    public int getLlaveNumerica() {
+         this.llaveNumerica = procesarCadena(numeroControl);
+        return llaveNumerica;
+    }
+
     public Llave(){
         this.codigo="01";
+        this.numeroControl = "24070011";
     }
+    
 }
--- CuentaSegura/src/cuentasegura/LlavePrivada.java
+++ CuentaSegura/src/cuentasegura/LlavePrivada.java
@@ -2,12 +2,26 @@ package cuentasegura;
 
 /**
  *
- * @author Carlos
+ * @author Carlos y modificado por armando
  */
-public class LlavePrivada extends Llave{
+public class LlavePrivada {
+    
+    
     private int llaveNumerica;
+     private String numeroControl;
     
     public LlavePrivada(){
         this.llaveNumerica = 0;
+        
+        
+    }
+    
+    public static void main(String[] args) {
+        Llave  A = new Llave();
+        String numeroControl = "24070011"; // Cambia esto por tu número de control real
+        
+       A.generarLlave(numeroControl);
+        
+        System.out.println("La llave numérica generada es: " + A.getLlaveNumerica());
     }
 }
