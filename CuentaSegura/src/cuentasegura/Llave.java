cdcc92ee8344851969f3f4eabe1041e505d19378
Armandobs12 <l24070011@huetamo.tecnm.mx>
2025-03-31 07:38:28 -0600

    se copio el repositorio
    pase el archivo de cuenta segura a mi repositorio para obtener mi llave

---
+++ CuentaSegura/src/cuentasegura/Llave.java
@@ -0,0 +1,17 @@
+package cuentasegura;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Llave {
+    private String codigo;
+    
+    public String generarLlave(String codigo){
+        return this.codigo;
+    }
+    
+    public Llave(){
+        this.codigo="01";
+    }
+}
---
+++ CuentaSegura/src/cuentasegura/LlavePrivada.java
@@ -0,0 +1,13 @@
+package cuentasegura;
+
+/**
+ *
+ * @author Carlos
+ */
+public class LlavePrivada extends Llave{
+    private int llaveNumerica;
+    
+    public LlavePrivada(){
+        this.llaveNumerica = 0;
+    }
+}
